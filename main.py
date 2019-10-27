a01ab5cc261662a7f1ad966906ba2138a3cc2fbe
Ege Hatırnaz <egehatirnaz@hotmail.com>
2019-10-27 22:04:18 +0300

    Not working version with a*

--- main.py
+++ main.py
@@ -1,46 +1,31 @@
 from anytree import Node, RenderTree
 import heapq
 from builder import Builder
+from States import States
+import queue as Q
 
 
-class PriorityQueue:
-    def _init_(self):
-        self.queue = []
-
-    def is_empty(self):
-        return len(self.queue) == 0
-
-    def put(self, state, priority):
-        heapq.heappush(self.queue, (priority, state))
-
-    def pop(self):
-        heapq.heappop(self)
-
-
-def heuristic_func(goal, child):
-    return goal  # degisecek
-
-
-def a_star(anytree, root, goal):
-    queue = PriorityQueue()
-    queue.put(root, 0)
+def a_star(root, goal):
+    # Form a one-element queue consisting of a zero length path that contains only the root node.
+    queue = Q.Queue()
+    queue.put(root)
     path = {}
     path_weights = {}
     path[root] = None
     path_weights[root] = 0
 
-    while not anytree.is_empty:
-        n = queue.pop()
+    while not queue.empty():
+        n = queue.get()
 
         if n == goal:
             break
 
         for child in n.children:
             new_weight = path_weights[n] + 1  # every level costs 1
-            if child not in path_weights or new_weight < path_weights[child]:
+            if child not in path_weights: # or new_weight < path_weights[child]:
                 path_weights[child] = new_weight
-                priority = new_weight + heuristic_func(goal, child)
-                queue.put(child, priority)
+                priority = new_weight # heuristic_func(goal, child)
+                queue.put(child)
                 path[child] = n
 
     return path, path_weights
@@ -48,8 +33,10 @@ def a_star(anytree, root, goal):
 
 def main():
     b = Builder(6, 6, 1)
+    goal = States(0,0,0, [])
     root = b.build_tree()
-    print(RenderTree(root))
+    #print(RenderTree(root))
+    print(a_star(root, goal))
 
 if __name__ == '__main__':
     main()
